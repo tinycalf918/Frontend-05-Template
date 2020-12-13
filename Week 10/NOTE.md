44064326909c136ef9acf2a3a0187c0d7f3def0e
tinycalf <386829579@qq.com>
2020-12-13 11:35:24 +0800

    week 10

--- Week 10/NOTE.md
+++ Week 10/NOTE.md
@@ -1,1 +1,23 @@
 学习笔记
+flex 分行  按主轴进行分行  如果设置了nowrap则强行放到第一行
+
+主轴计算
+找出flex元素
+把主轴剩余尺寸按flex比例分配
+如果剩余空间为负数，把所有flex元素设为0，然后等比压缩剩余元素
+没有flex元素则根据justify-content来计算
+
+计算交叉轴
+根据flex-align和item-align属性设置
+
+绘制单个元素
+绘制需要依赖一个图形环境
+采用npm包images
+绘制在一个viewport上进行
+与绘制相关的属性：background-color、border、background-image等。gradient需要WebGL相关的库来处理
+
+绘制DOM
+递归调用子元素的绘制方法完成DOM树的绘制
+忽略一些不需要绘制的节点
+实际浏览器中，文字绘制是难点，需要依赖字体库，把字体变成图片去渲染
+实际浏览器中，还会对一些图层做compositing
---
+++ Week 10/toy-browser/server.js
@@ -0,0 +1,46 @@
+const http = require('http');
+
+http.createServer((request, response) => {
+    let body = [];
+    request.on('error', (err) =>{
+        console.error(err);
+    }).on('data', (chunk) => {
+        body.push(chunk.toString());
+    }).on('end', () => {
+        // body = Buffer.concat(body).toString();
+        console.log("111body:", body);
+        response.writeHead(200, {'Content-Type': 'text/html'});
+        rresponse.end(`
+        <html maaa=a>
+        <head>
+          <style>
+            #container {
+              width: 500px;
+              height: 300px;
+              display: flex;
+              background-color: rgb(255, 255, 255);
+            }
+            #container #myid {
+              width: 200px;
+              height: 100px;
+              background-color: rgb(255, 0, 0);
+            }
+            #container .c1 {
+              flex: 1;
+              background-color: rgb(0, 255, 0);
+            }
+          </style>
+        </head>
+        <body>
+          <div id="container">
+            <div id="myid" />
+            <div class="c1" />
+          </div>
+        </body>
+      </html>`);
+        });
+    }).listen(8088, () => {
+        console.log('Server started on port: 8088')
+    });
+
+console.log("server started");
